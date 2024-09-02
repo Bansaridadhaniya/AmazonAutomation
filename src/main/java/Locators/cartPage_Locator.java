f46cba1a5ff661791108163fa9a16bef0c04ab8e
Bansari Dadhaniya <bansidadhaniya2903@gmail.com>
2024-09-02 10:36:24 +0530

    Revert "login,search and add to cart"
    This reverts commit 6b2714c1b7155293b34d12d40d2e42dfeb15a208.

--- src/main/java/Locators/cartPage_Locator.java
+++
@@ -1,7 +0,0 @@
-package Locators;
-
-import org.openqa.selenium.By;
-
-public class cartPage_Locator {
-    public static final By AddToCartButton = By.id("nav-cart");
-}
--- src/main/java/Locators/listingPage_Locator.java
+++
@@ -1,8 +0,0 @@
-package Locators;
-
-import org.openqa.selenium.By;
-
-public class listingPage_Locator {
-    public static final By First_item = By.id("a-autoid-2-announce");
-
-}
